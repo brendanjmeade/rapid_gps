c54c65f71f1b4a899c9b8fec1ad97a5ed4d34889
amyferrick <47235938+amyferrick@users.noreply.github.com>
2020-07-01 07:29:43 -0700

    Update map_displcements.py
    Improved visualization of plots. Still need to make the limits of the colormap consistent.

--- map_displacements.py
+++ map_displacements.py
@@ -29,7 +29,7 @@ max_lon = -114
 socal = query_locations(min_lat, max_lat, min_lon, max_lon)
 
 # read in the gps data
-df = dd.read_parquet('/Users/amyferrick/Downloads/5_min_GPS_subset/unr_5min_gps.parquet')
+df = dd.read_parquet('/Users/amyferrick/Downloads/test2015.parquet')
 # only choose the sites that are in the desired region
 df = df[df['site'].isin(socal['site'])]
 # add the lat and lon columns to the gps dataframe
@@ -37,9 +37,9 @@ df = df.join(socal.set_index('site'), on='site')
 
 
 
-# get data at a certain time
-# start = 'datetime.datetime(2015, 1, 11, 2, 5, 0)'
-# df = df.query('date_time == ' + start)
+# get data at a single 5 minute interval
+start = 'datetime.datetime(2015, 1, 11, 2, 5, 0)'
+df = df.query('date_time == ' + start)
 df = df.compute()
 
 
@@ -55,16 +55,17 @@ grid_e = griddata(points, df['e_ref'], (grid_x, grid_y), method='linear')
 grid_n = griddata(points, df['n_ref'], (grid_x, grid_y), method='linear')
 grid_v = griddata(points, df['v_ref'], (grid_x, grid_y), method='linear')
 
-plt.subplot(131)
-plt.imshow(grid_e.T, extent=(min_lon, max_lon, min_lat, max_lat), origin='lower')
-plt.plot(points_X, points_Y, "k.", ms=.25)
-plt.title('East Displacement')
-plt.subplot(132)
-plt.imshow(grid_n.T, extent=(min_lon, max_lon, min_lat, max_lat), origin='lower')
-plt.plot(points_X, points_Y, "k.", ms=.25)
-plt.title('North Displacement')
-plt.subplot(133)
-plt.imshow(grid_v.T, extent=(min_lon, max_lon, min_lat, max_lat), origin='lower')
-plt.plot(points_X, points_Y, "k.", ms=.25)
-plt.title('Up Displacement')
+fig, axs = plt.subplots(1, 3, figsize=(10,8))
+axs[0].imshow(grid_e.T, cmap='RdYlGn', extent=(min_lon, max_lon, min_lat, max_lat), origin='lower')
+axs[0].plot(points_X, points_Y, "k.", ms=1)
+axs[0].set_title('East Displacement', weight='bold')
+axs[0].set_ylabel('Latitude', weight='bold')
+axs[1].imshow(grid_n.T, cmap='RdYlGn', extent=(min_lon, max_lon, min_lat, max_lat), origin='lower')
+axs[1].plot(points_X, points_Y, "k.", ms=1)
+axs[1].set_title('North Displacement', weight='bold')
+axs[1].set_xlabel('Longitude', weight='bold')
+axs[2].imshow(grid_v.T, cmap='RdYlGn', extent=(min_lon, max_lon, min_lat, max_lat), origin='lower')
+axs[2].plot(points_X, points_Y, "k.", ms=1)
+axs[2].set_title('Up Displacement', weight='bold')
+fig.colorbar(axs[0].imshow(grid_e.T, cmap='RdYlGn', extent=(min_lon, max_lon, min_lat, max_lat), origin='lower'), ax=axs, orientation='horizontal')
 plt.show()
