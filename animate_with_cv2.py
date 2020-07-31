26e654eea1f93cb4e0105a21677646439ed41aed
ArielEspindola-Mercado <66691325+ArielEspindola-Mercado@users.noreply.github.com>
2020-07-31 11:24:08 -0700

    Update animate_with_cv2.py

--- animate_with_cv2.py
+++ animate_with_cv2.py
@@ -1,11 +1,11 @@
 import cv2
 import glob
 import os
-# import numpy as np
+
 
 FILE_PATH = 'E:/Pictures/2015_Slideshow(limited_normalized_week)'
 
-img_array = []
+img_list = []
 print('Beginning to Glob')
 # The glob is sorted based on the time of creation
 file_names = sorted((glob.glob(FILE_PATH +'/*.png')), key=os.path.getmtime)
@@ -15,11 +15,11 @@ for filename in file_names:
     img = cv2.imread(filename)
     height, width, layers = img.shape
     size = (width, height)
-    img_array.append(img)
+    img_list.append(img)
 
 
 out = cv2.VideoWriter('E:/Pictures/mp4s_of_project/2015_week_1.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 30, size)
 
-for i in range(len(img_array)):
-    out.write(img_array[i])
+for i in range(len(img_list)):
+    out.write(img_list[i])
 out.release()
